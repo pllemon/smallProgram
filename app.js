e6a947184f9528dfb4429aeef7ed3904c88695a6
pllemon <nageshui071011>
2019-07-30 00:26:04 +0800

    更新

--- app.js
+++ app.js
@@ -2,15 +2,6 @@
 import api from './utils/api'
 App({
   onLaunch: function () {
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        this.api.wxappLogin(res.code).then(res => {
-          console.log(res)
-        })
-      }
-    })
     // 获取用户信息
     wx.getSetting({
       success: res => {
