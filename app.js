de9204ba4e35d50ada2f83a1d6c0b7503a09ed5b
13650964250@163.com <nageshui071011>
2019-07-29 16:30:40 +0800

    更新

--- app.js
+++ app.js
@@ -2,14 +2,10 @@
 import api from './utils/api'
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
+        console.log(res);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,26 +1,75 @@
-// pages/my/my.js
 const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    userInfo: {},
+    hasUserInfo: false,
+    navList: [
+      {
+        text: '我的答卷',
+        icon: 'icon_compile',
+        url: '/pages/question/question'
+      },
+      {
+        text: '我的二维码',
+        icon: 'icon_QRcode',
+        url: '/pages/question/question'
+      },
+      {
+        text: '我的下级',
+        icon: 'icon_group',
+        url: '/pages/subordinate/subordinate'
+      },
+      {
+        text: '消费记录',
+        icon: 'icon_coinpurse_line',
+        url: '/pages/subordinate/subordinate'
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    return false;
-    let that = this
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  loadMethods: function () {
     wx.showLoading({
       title: '加载中...',
     })
     wx.login({
       success(res) {
         app.api.wxappLogin(res.code).then(res => {
+          console.log(res)
           wx.hideLoading()
         })
       },
@@ -29,58 +78,13 @@ Page({
       }
     })
   },
-  goToUrl(e) {
-    // app.goPage(url) 
-    wx.navigateTo({
-      url: e.target.dataset.url,
+
+  scanCode: function () {
+    wx.scanCode({
+      success (res) {
+        console.log(res)
+        loadMethods();
+      }
     })
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
   }
 })
